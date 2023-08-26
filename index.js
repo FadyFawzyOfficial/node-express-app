d6965ea7f2403a9577912b5774ae788b9ee154dc
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-26 21:34:45 +0300

    7. Handle HTTP GET Requests

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "Course 1" },
+  { id: 2, name: "Course 2" },
+  { id: 3, name: "Course 3" },
+];
+
 app.get("/", (request, response) => response.send("Hello World!!!"));
 
-app.get("/api/courses", (request, response) => response.send([1, 2, 3]));
+app.get("/api/courses", (request, response) => response.send(courses));
 
-app.get("/api/posts/:id", (request, response) =>
-  response.send(request.params.id)
-);
+app.get("/api/courses/:id", (request, response) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(request.params.id)
+  );
+  if (!course)
+    response.status(404).send("The course with the given id was not found");
+  response.send(course);
+});
 
 // app.get("/api/posts/:year/:month", (request, response) =>
 //   response.send(request.params)
