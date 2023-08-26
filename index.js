aa8d42261eee94b65ef3ce7832e6ce66656ddb98
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-08-26 21:17:32 +0300

    6. Understand Route Parameters

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ app.get("/", (request, response) => response.send("Hello World!!!"));
 
 app.get("/api/courses", (request, response) => response.send([1, 2, 3]));
 
+app.get("/api/posts/:id", (request, response) =>
+  response.send(request.params.id)
+);
+
+// app.get("/api/posts/:year/:month", (request, response) =>
+//   response.send(request.params)
+// );
+
+app.get("/api/posts/:year/:month", (request, response) =>
+  response.send(request.query)
+);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
